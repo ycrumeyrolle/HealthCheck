6af44f08b2c32d9f7f6857fc5b9ee5d81d0ed654
Simon Dib <dib.simon@gmail.com>
2017-11-20 23:06:17 +0100

    feat(mongo-check): Add mongo check

---
+++ src/AspNetCore.HealthCheck.Mongo/MongoCheck.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace AspNetCore.HealthCheck.Mongo
+{
+    public class MongoCheck : HealthCheck<MongoSettings>
+    {
+        public override async Task CheckHealthAsync(HealthCheckContext context, MongoSettings settings)
+        {
+            IAsyncCursor<BsonDocument> asyncCursor = await settings.Database.ListCollectionsAsync(null, context.CancellationToken);
+            await asyncCursor.ToListAsync();
+            context.Succeed();
+        }
+    }
+}
+
---
+++ src/AspNetCore.HealthCheck.Mongo/MongoOptions.cs
@@ -0,0 +1,7 @@
+﻿namespace AspNetCore.HealthCheck.Mongo
+{
+    public class MongoOptions : CheckOptions
+    {
+        public string ConnectionString { get; set; }
+    }
+}
